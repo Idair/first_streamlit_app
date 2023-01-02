94410de8aa1ed2abb22b04aaa9696829d7f78269
Idair <87037705+Idair@users.noreply.github.com>
2023-01-01 23:50:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
-import snowflake.connector
+
 import streamlit
 import pandas
 import requests
-
+import snowflake.connector
+from urllib.error import URLError
 
 # https://idair-first-streamlit-app-streamlit-app-l8ibwu.streamlit.app/
 
@@ -33,33 +34,56 @@ streamlit.dataframe(fruits_to_show)
 #--------------------------------------------------------------
 
 streamlit.header("Fruityvice Fruit Advice!")
-#Allow user to enter fruit name, capture entry and pass to API
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-                
-#streamlit.text(fruityvice_response.json())  #displays the json data as is 
-
-# Normalize the data  
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display data in a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  #Allow user to enter fruit name, capture entry and pass to API
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.write('Please select a fruit for information.')
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Normalize the data  
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # display data in a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error() 
 
 #https://docs.google.com/spreadsheets/d/1hLBv0pO1qpeMtUncE8Yv1NSdqaDrY9CqmXoP5b9313s/edit?resourcekey#gid=663712744
 
+#-----------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------Snowflake connection ----------------------------------------------
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#-----------------------------------------------------------------------------------------------------------------------
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()  #Fetch all rows not just one
 streamlit.header("Fruit Load List Contains:")
-streamlit.dataframe(my_data_rows)
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()  #Fetch all rows not just one
+
+#Add a button to load the fruit 
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list() 
+  streamlit.dataframe(my_data_rows)
+    
+#-----------------------------------------------------------------------------------------------------------------------
 #Allow user to enter fruit name, capture entry and pass to API
-add_fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_fruit_choice)
+#-----------------------------------------------------------------------------------------------------------------------
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur: 
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit +"')")  
+    return 'Thanks for adding ' + new_fruit 
+  
+add_fruit_choice = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  function_return_val = insert_row_snowflake(add_fruit_choice) 
+  streamlit.text(function_return_val)    
+  
+  
+  
